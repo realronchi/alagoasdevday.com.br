37123f26e9ccc74715df7eb80552ca12c5eb101b
Ivan Santos <pragmaticivan@gmail.com>
2015-02-15 19:07:19 -0600

    Test ismobile function

--- app/scripts/playground.js
+++ app/scripts/playground.js
@@ -1,26 +1,7 @@
 /*global $:false */
 'use strict';
 
-
-function detectmob() {
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
-
-    return true;
-  }
- else {
-    return false;
-  }
-}
-
-
-
+(function(i){var e=/iPhone/i,n=/iPod/i,o=/iPad/i,t=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,r=/Android/i,d=/BlackBerry/i,s=/Opera Mini/i,a=/IEMobile/i,b=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,h=RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),c=function(i,e){return i.test(e)},l=function(i){var l=i||navigator.userAgent;this.apple={phone:c(e,l),ipod:c(n,l),tablet:c(o,l),device:c(e,l)||c(n,l)||c(o,l)},this.android={phone:c(t,l),tablet:!c(t,l)&&c(r,l),device:c(t,l)||c(r,l)},this.other={blackberry:c(d,l),opera:c(s,l),windows:c(a,l),firefox:c(b,l),device:c(d,l)||c(s,l)||c(a,l)||c(b,l)},this.seven_inch=c(h,l),this.any=this.apple.device||this.android.device||this.other.device||this.seven_inch},v=i.isMobile=new l;v.Class=l})(window);
 
 
 $(document).ready(function($){
@@ -108,7 +89,7 @@ function Circle() {
 }
 
 $(document).ready(function(){
-if (!detectmob()) {
+if (!isMobile.any) {
   WIDTH = window.innerWidth;
   HEIGHT = window.innerHeight;
   canvas = document.getElementById('pixie');
