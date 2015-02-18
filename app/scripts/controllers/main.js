0489ebd928a13bfbe3954ee294f9a6d7b01e1e4d
Gustavo Cunha <gustavocunha@gmail.com>
2015-02-18 16:56:21 -0300

    Move content to different controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,37 +7,58 @@
  * # MainCtrl
  * Controller of the alagoasdevdaycombrApp
  */
-angular.module('alagoasdevdaycombrApp', ['ngResource'])
-  .controller('MainCtrl', function ($scope, $resource) {
-    var baseURL = 'http://admin.alagoasdevday.com.br/api/v1/:action';
+var app = angular.module('alagoasdevdaycombrApp', ['ngResource']);
+var baseURL = 'http://admin.alagoasdevday.com.br/api/v1/:action';
 
-    // get speakers
-    $scope.speakers = $resource(
-      baseURL,
-      {
-        action: 'speakers.json',
-        callback: 'JSON_CALLBACK'
-      },
-      {
-        get: {
-          method: 'JSONP'
-        }
-      }
-    );
-    $scope.speakerResults = $scope.speakers.get();
+app.controller('MainCtrl', function () {
+});
 
-    // get sponsors
-    $scope.sponsors = $resource(
-      baseURL,
-      {
-        action: 'sponsors.json',
-        callback: 'JSON_CALLBACK'
-      },
-      {
-        get: {
-          method: 'JSONP'
-        }
+/**
+ * @ngdoc function
+ * @name alagoasdevdaycombrApp.controller:SpeakersCtrl
+ * @description
+ * # SpeakersCtrl
+ * Controller of the alagoasdevdaycombrApp
+ */
+app.controller('SpeakersCtrl', function ($scope, $resource) {
+  // get speakers
+  $scope.speakers = $resource(
+    baseURL,
+    {
+      action: 'speakers.json',
+      callback: 'JSON_CALLBACK'
+    },
+    {
+      get: {
+        method: 'JSONP'
       }
-    );
-    $scope.sponsorResults = $scope.sponsors.get();
-  });
+    }
+  );
+  $scope.speakerResults = $scope.speakers.get();
+});
+
+/**
+ * @ngdoc function
+ * @name alagoasdevdaycombrApp.controller:SponsorsCtrl
+ * @description
+ * # SponsorsCtrl
+ * Controller of the alagoasdevdaycombrApp
+ */
+app.controller('SponsorsCtrl', function ($scope, $resource) {
+
+  // get sponsors
+  $scope.sponsors = $resource(
+    baseURL,
+    {
+      action: 'sponsors.json',
+      callback: 'JSON_CALLBACK'
+    },
+    {
+      get: {
+        method: 'JSONP'
+      }
+    }
+  );
+  $scope.sponsorResults = $scope.sponsors.get();
+});
+
