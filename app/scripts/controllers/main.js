f37ae98a141d6f842b786b0c7e1fd4020aad107f
Gustavo Cunha <gustavocunha@gmail.com>
2015-02-18 09:28:27 -0300

    Add Angular do get information from API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,37 @@
  * # MainCtrl
  * Controller of the alagoasdevdaycombrApp
  */
-angular.module('alagoasdevdaycombrApp')
-  .controller('MainCtrl', function () {
+angular.module('alagoasdevdaycombrApp', ['ngResource'])
+  .controller('MainCtrl', function ($scope, $resource) {
+    var baseURL = 'http://admin.alagoasdevday.com.br/api/v1/:action';
 
+    // get speakers
+    $scope.speakers = $resource(
+      baseURL,
+      {
+        action: 'speakers.json',
+        callback: 'JSON_CALLBACK'
+      },
+      {
+        get: {
+          method: 'JSONP'
+        }
+      }
+    );
+    $scope.speakerResults = $scope.speakers.get();
+
+    // get sponsors
+    $scope.sponsors = $resource(
+      baseURL,
+      {
+        action: 'sponsors.json',
+        callback: 'JSON_CALLBACK'
+      },
+      {
+        get: {
+          method: 'JSONP'
+        }
+      }
+    );
+    $scope.sponsorResults = $scope.sponsors.get();
   });
