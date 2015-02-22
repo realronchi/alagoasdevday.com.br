fe6eef90f9bdc58294176c22fee343239a30c622
Ivan Santos <pragmaticivan@gmail.com>
2015-02-21 20:31:10 -0600

    Rockets update

--- app/scripts/playground.js
+++ app/scripts/playground.js
@@ -48,8 +48,8 @@ $(document).ready(function($){
 
   var tlRocket = new TimelineMax({delay:1.5, repeat:50, onComplete:oviniFloat});
 
-  tlRocket.to($rocket1, 3, {top: -1000, left: 750,  ease: Power3.easeInOut}, "+=1.0");
-  tlRocket.to($rocket2, 3, {top: -1000, left: 750,  ease: Power3.easeInOut}, "+=1.0");
+  tlRocket.to($rocket1, 3, {top: -1000, left: 600,  ease: Power3.easeInOut}, "+=1.0");
+  tlRocket.to($rocket2, 3, {top: -1000, left: -950,  ease: Power3.easeInOut}, "+=1.0");
   tlRocket.to($rocket3, 3, {top: -1000, left: 750,  ease: Power3.easeInOut}, "+=1.0");
 
 
